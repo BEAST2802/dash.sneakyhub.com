337953115ca75d456857ecd3c3ecf5eea42b70f0
Sneaky <78713939+SneakyHub@users.noreply.github.com>
2022-07-11 00:44:31 +0100

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -107,6 +107,20 @@
                             {{ __('Useful Links') }}
                         </h3>
                     </div>
+                    <div class="mt-1">
+                                                    <span class="badge badge-success"><i
+                                                            class="fa fa-user-check mr-2"></i>
+                                                            <p>Your Referral Link</p>
+                                                        {{_("Referral URL")}} :
+                                                        <span onclick="onClickCopy()" id="RefLink" style="cursor: pointer;">
+                                                            {{route("register")}}?ref={{$user->referral_code}}</span>
+                                                    </span>
+                                                @else
+                                                    <span class="badge badge-warning"><i
+                                                            class="fa fa-user-check mr-2"></i>
+                                                        {{_("Make a purchase to reveal your referral-URL")}}</span>
+                                        @endif
+                    </div>
                     <!-- /.card-header -->
                     <div class="card-body">
                         @foreach ($useful_links as $useful_link)
