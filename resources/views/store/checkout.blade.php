224967b06c984a03951357922e25e57d331227a2
Sneaky <78713939+SneakyHub@users.noreply.github.com>
2022-07-11 00:22:08 +0100

    Update checkout.blade.php

--- resources/views/store/checkout.blade.php
+++ resources/views/store/checkout.blade.php
@@ -157,6 +157,18 @@
                 </div><!-- /.col -->
             </div><!-- /.row -->
         </div>
+    <!-- AD CONTENT -->
+    <script type="text/javascript">
+    	atOptions = {
+    		'key' : '9248df68e86c51704b8d4cdbab8d1453',
+    		'format' : 'iframe',
+    		'height' : 90,
+    		'width' : 728,
+    		'params' : {}
+    	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.topdisplayformat.com/9248df68e86c51704b8d4cdbab8d1453/invoke.js"></scr' + 'ipt>');
+            </script>
+    <!-- END AD CONTENT -->
     </section>
     <!-- END CONTENT -->
 
