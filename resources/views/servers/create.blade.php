64b799bf5b66965d7c1ad4871c18705a36c8ce4d
Sneaky <78713939+SneakyHub@users.noreply.github.com>
2022-07-11 00:27:58 +0100

    Update create.blade.php

--- resources/views/servers/create.blade.php
+++ resources/views/servers/create.blade.php
@@ -30,6 +30,18 @@
                 @csrf
                 <div class="col-xl-6 col-lg-8 col-md-8 col-sm-10">
                     <div class="card">
+    <!-- AD CONTENT -->
+    <script type="text/javascript">
+    	atOptions = {
+    		'key' : '9248df68e86c51704b8d4cdbab8d1453',
+    		'format' : 'iframe',
+    		'height' : 90,
+    		'width' : 728,
+    		'params' : {}
+    	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.topdisplayformat.com/9248df68e86c51704b8d4cdbab8d1453/invoke.js"></scr' + 'ipt>');
+            </script>
+    <!-- END AD CONTENT -->
                         <div class="card-header">
                             <div class="card-title"><i class="fas fa-cogs mr-2"></i>{{ __('Server configuration') }}
                             </div>
