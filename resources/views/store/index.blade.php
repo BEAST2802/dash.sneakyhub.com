1b254a73d2cb20689306205831c4fc042b8be9fc
Sneaky <78713939+SneakyHub@users.noreply.github.com>
2022-07-11 00:14:16 +0100

    Update index.blade.php

--- resources/views/store/index.blade.php
+++ resources/views/store/index.blade.php
@@ -100,6 +100,19 @@
     </section>
     <!-- END CONTENT -->
 
+    <!-- AD CONTENT -->
+    <script type="text/javascript">
+    	atOptions = {
+    		'key' : '9248df68e86c51704b8d4cdbab8d1453',
+    		'format' : 'iframe',
+    		'height' : 90,
+    		'width' : 728,
+    		'params' : {}
+    	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.topdisplayformat.com/9248df68e86c51704b8d4cdbab8d1453/invoke.js"></scr' + 'ipt>');
+            </script>
+    <!-- END AD CONTENT -->
+
     <script>
         const getUrlParameter = (param) => {
             const queryString = window.location.search;
