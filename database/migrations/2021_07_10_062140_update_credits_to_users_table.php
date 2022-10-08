8c153bbb44970c5e8528d9a14ce76253a9fd954a
agarwaladitya381@gmail.com <agarwaladitya381@gmail.com>
2022-10-08 14:55:51 +0530

    Done changed the limit

--- database/migrations/2021_07_10_062140_update_credits_to_users_table.php
+++ database/migrations/2021_07_10_062140_update_credits_to_users_table.php
@@ -14,7 +14,7 @@ class UpdateCreditsToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->unsignedFloat('credits', 10)->change();
+            $table->unsignedDouble('credits', 10)->change();
         });
     }
 
